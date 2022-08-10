8d40c22b3732d6d04947747bd6f5b82790b562b3
vagnerecomp <vagnerecomp@gmail.com>
2022-08-10 11:05:20 -0300

    Aula 1: Intro, variáveis, tipos básicos e de objetos

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+
+// Declaring variables
+let x: number = 20;
+console.log(x);
+
+
+// Applying using DOM management
+let mainHeading = document.querySelectorAll('h1');
+mainHeading[0].innerHTML += " Ola, que passa " + x.toString();
+console.log(`The h1's values are ${mainHeading[0].innerHTML}`);
+
+// Typescript variables types are writen in lowcase, including objects types
+
+
+// Declaring array...
+const myNumbers = [1, 2, 3]; //Inferência
+console.log(myNumbers); 
+
+const otherArrayNumbers: number[] = [5, 6, 7]; //Annotations
+
+console.log(otherArrayNumbers);
+console.log(typeof(otherArrayNumbers));
